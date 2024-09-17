38f952e9c5d35f212023c0e9c76c04ca16a6b922
Mateus Oliveira <mateus.m.oliveira2303@gmail.com>
2024-09-17 14:07:04 -0300

    terminado crud

--- 4-Banco-de-dados/Aula1/main.go
+++ 4-Banco-de-dados/Aula1/main.go
@@ -6,21 +6,18 @@ import (
 
 	// O pacote sql fornece uma interface genérica para interagir com diferentes bancos de dados, neste caso, o MySQL.
 
-	"github.com/google/uuid"
-
 	_ "github.com/go-sql-driver/mysql"
 	// Este é o driver específico para MySQL, necessário para que Go possa interagir com o banco de dados MySQL.
 )
 
 type Product struct {
-	ID    string
+	ID    int
 	Name  string
 	Price float64
 }
 
 func NewProduct(name string, price float64) *Product {
 	return &Product{
-		ID:    uuid.New().String(),
 		Name:  name,
 		Price: price,
 	}
@@ -42,13 +39,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	product := NewProduct("Celular", 1500.0)
-	err = InsertProduct(db, *product)
+
+	product := NewProduct("Carregador", 150.0)
+	err = InsertProduct(db, product)
 	if err != nil {
 		panic(err)
 	}
-	product.Price = 2000.0
-	err = UpdateProduct(db, *product)
+	fmt.Printf("Produto inserido com ID: %d\n", product.ID)
+
+	product.Price = 100.0
+	product.Name = "Carregador iphone"
+	err = UpdateProduct(db, product)
 	if err != nil {
 		panic(err)
 	}
@@ -60,7 +61,7 @@ func main() {
 }
 
 func CreateTable(db *sql.DB) error {
-	_, err := db.Exec("CREATE TABLE Products (id varchar(100), name varchar(100), price decimal(10, 2));")
+	_, err := db.Exec("CREATE TABLE products (id INT AUTO_INCREMENT PRIMARY KEY, name varchar(100), price decimal(10, 2));")
 	// Executa uma consulta SQL
 	if err != nil {
 		panic(err)
@@ -68,8 +69,8 @@ func CreateTable(db *sql.DB) error {
 	return nil
 }
 
-func InsertProduct(db *sql.DB, product Product) error {
-	query, err := db.Prepare("INSERT INTO products(id, name, price) VALUES (?, ?, ?)")
+func InsertProduct(db *sql.DB, product *Product) error {
+	query, err := db.Prepare("INSERT INTO products(name, price) VALUES (?, ?);")
 	// prepara uma instrução SQL para ser executada várias vezes com diferentes valores. Ela é útil quando deseja executar a mesma consulta repetidamente, trocando apenas os parâmetros.
 	// é mais eficiente para consultas repetidas, porque a consulta SQL é analisada (parsed) apenas uma vez pelo banco de dados, e os parâmetros são fornecidos posteriormente nas execuções.
 	// Os pontos de interrogação (?) na string SQL de db.Prepare são placeholders que indicam onde os valores dos parâmetros serão inseridos.
@@ -78,31 +79,56 @@ func InsertProduct(db *sql.DB, product Product) error {
 		return err
 	}
 	defer query.Close()
-	_, err = query.Exec(product.ID, product.Name, product.Price)
+
+	result, err := query.Exec(product.Name, product.Price)
 	// executa diretamente uma instrução SQL (como INSERT, UPDATE, ou DELETE), sem a necessidade de prepará-la previamente. Essa função é útil para consultas que você não precisa reutilizar.
 	// os argumentos fornecidos são mapeados diretamente, na ordem, para os placeholders na string do argumento da função db.Prepare.
 
 	if err != nil {
 		return err
 	}
+
+	lastInsertId, err := result.LastInsertId()
+	// retorna o inteiro gerado pelo banco de dados em resposta a um comando. Normalmente, isso ocorrerá em uma coluna de "incremento automático" ao inserir uma nova linha.
+
+	if err != nil {
+		return err
+	}
+
+	product.ID = int(lastInsertId)
+	fmt.Printf("ID: %v\nName: %v\nPrice: %v\n", product.ID, product.Name, product.Price)
 	return nil
 }
 
-func UpdateProduct(db *sql.DB, product Product) error {
-	stmt, err := db.Prepare("UPDATE products set name = ?, price = ?, where id = ?")
+func UpdateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("UPDATE products SET name = ?, price = ? where id = ?;")
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
-	_, err = db.Exec(product.Name, product.Price, product.ID)
+
+	result, err := stmt.Exec(product.Name, product.Price, product.ID)
 	if err != nil {
 		return err
 	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		println("Nenhuma linha foi atualizada, verifique o ID")
+		return nil
+	}
+
+	fmt.Printf("Atualizando produto:\nID:%v\nName:%v\nPrice:%.2f\n", product.ID, product.Name, product.Price)
+
 	return nil
 }
 
-func SelectProduct(db *sql.DB, id string) (*Product, error) {
-	stmt, err := db.Prepare("SELECT id, name, price from products where id = ?")
+func SelectProduct(db *sql.DB, id int) (*Product, error) {
+	stmt, err := db.Prepare("SELECT id, name, price from products where id = ?;")
 	if err != nil {
 		return nil, err
 	}
